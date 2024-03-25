d4ef4483be5623ee35706a83bab86c912a3abaa2
Toluwalase Abigail <toluabby12@gmail.com>
2024-03-25 10:47:50 +0100

    Springboot initial commit

---
+++ src/main/java/com/deals/isodeals/IsoDealsApplication.java
@@ -0,0 +1,13 @@
+package com.deals.isodeals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsoDealsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsoDealsApplication.class, args);
+    }
+
+}
